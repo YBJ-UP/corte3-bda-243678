25872d2576ecffd49fff0ec511556e8b103acedd
Yael <243678@ids.upchiapas.edu.mx>
2026-04-20 00:17:34 -0600

    ya medio funciona el docker, sigue tronando pero por otras razones :(

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from api!")
+from fastapi import FastAPI
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+@app.get(path="/health")
+def health():
+    return { "success": True, "message": "Api en ejecución" }
