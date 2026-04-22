e2d4a5be8b08d33117055c535af174c4bb950c11
Yael <243678@ids.upchiapas.edu.mx>
2026-04-21 18:30:27 -0600

    ya me di cuenta de que el archivo user era redundante, fuera

--- api/queries/userQueries.py
+++ api/queries/userQueries.py
@@ -4,19 +4,20 @@ from psycopg_pool import ConnectionPool
 from redis import Redis
 
 from model.response import DeleteResponse, PatchResponse, Response
-from queries.user import User
+from queries.baseQueries import BaseQueries
 
-class UserQueries(User):
+class UserQueries(BaseQueries): # wrapper para ocultar los roles de los endpoints
 
     def __init__(self, pg_pool: ConnectionPool, redis_client: Redis, role: Literal['Administrador', 'Veterinario', 'Recepcionista']) -> None:
         super().__init__(pg_pool= pg_pool, redis_client= redis_client)
         self.ROLE: Literal['Administrador'] | Literal['Veterinario'] | Literal['Recepcionista'] = role
 
     def wipeAllCacheWrapper(self): # a canijo le di enter y me lo autocompletó
-        return self._wipeAllCacheWrapper(self.ROLE)
+        return self._wipeAllCache(self.ROLE) # se vuelve a poner por que así no pide el rol desde los endpoints
 
     def getAll[T](self, model: type[T], cachePrefix: str, tableAlias: str, query: str) -> Response[T]:
-        return self._getAll(
+        return self._get(
+            type= "all",
             model= model,
             cachePrefix=cachePrefix,
             tableAlias= tableAlias,
@@ -25,7 +26,8 @@ class UserQueries(User):
         )
 
     def getOne[T](self, model: type[T], cachePrefix: str, id: int, tableAlias: str, query: str) -> Response[T]:
-        return self._getOne(
+        return self._get(
+            type= "one",
             model= model,
             cachePrefix= cachePrefix,
             tableAlias= tableAlias,
@@ -35,7 +37,8 @@ class UserQueries(User):
         )
     
     def patch[T](self, model: type[T], cachePrefix: str, id: int, data: T, tableName: str) -> PatchResponse[T]:
-        return self._patch(
+        return self._add_or_patch(
+            isPatch= True,
             model= model,
             cachePrefix= cachePrefix,
             tableName= tableName,
@@ -55,7 +58,7 @@ class UserQueries(User):
         )
 
     def delete(self, cachePrefix: str, id: int, tableName: str, query: str) -> DeleteResponse:
-        return self._deleteWrap(
+        return self._delete(
             cachePrefix= cachePrefix,
             tableName= tableName,
             query= query,
