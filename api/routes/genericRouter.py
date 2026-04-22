8d512d885f37f4c3d9d36d60c47d3ace58f1b889
Yael <243678@ids.upchiapas.edu.mx>
2026-04-21 19:09:26 -0600

    no meti el mas importante al commit xd

--- api/routes/genericRouter.py
+++ api/routes/genericRouter.py
@@ -1,6 +1,32 @@
+from typing import Any, Type
+
 from fastapi import APIRouter
 
-def create_routes() -> APIRouter:
-    router = APIRouter()
-    # tu du ()
+from model.response import Response
+from queries.userQueries import UserQueries
+
+def create_routes(
+        user: UserQueries,
+        tableAlias: str,
+        tableName: str,
+        cachePrefix: str,
+        path: str,
+        read: Type[Any],
+        add: Type[Any],
+        patch: Type[Any],
+        readAllQuery: str,
+        readOneQuery: str,
+        deleteQuery: str
+) -> APIRouter:
+    router = APIRouter(prefix= path)
+
+    @router.get("/", response_model=Response[list[read]])
+    def getAll() -> Response[Any]:
+        return user.getAll(
+            model= read,
+            cachePrefix= cachePrefix,
+            tableAlias= tableAlias,
+            query= readAllQuery
+        )
+    
     return router
