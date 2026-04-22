810ceb103425bc8b943d0c98797003d601a10887
Yael <243678@ids.upchiapas.edu.mx>
2026-04-21 18:48:49 -0600

    tal vez si tengo tiempo de hacer el generador de rutas

--- api/routes/genericRouter.py
+++ api/routes/genericRouter.py
@@ -1,10 +1,6 @@
 from fastapi import APIRouter
 
-
-class router[T]:
-    def __init__(self, table: str) -> None:
-        self.router = APIRouter()
-        self.router.add_api_route(path=f"/test/{table}", endpoint=self.__getAll, methods=["GET"])
-
-    def __getAll(self):
-        return 1
+def create_routes() -> APIRouter:
+    router = APIRouter()
+    # tu du ()
+    return router
