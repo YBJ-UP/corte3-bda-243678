ba525e62c1de3249aabf55ffc448b670ba219889
Yael <243678@ids.upchiapas.edu.mx>
2026-04-23 21:47:32 -0600

    Extrae el tipo y lo envía al método get
    ya terminé este

--- src/app/api/[name]/route.ts
+++ src/app/api/[name]/route.ts
@@ -1,4 +1,7 @@
+import { Cita } from "@/interfaces/requests/dateRequest";
 import { Owner } from "@/interfaces/requests/ownerRequest";
+import { Pet } from "@/interfaces/requests/petRequest";
+import { Vaccine } from "@/interfaces/requests/vaccineRequest";
 import { Vet } from "@/interfaces/requests/vetRequest";
 import { get } from "@/lib/apiClient";
 import { NextRequest, NextResponse } from "next/server";
@@ -7,9 +10,9 @@ function getType(name: string) {
     switch (name){
         case "owner": return { nombre: "" } as Owner
         case "vet": return { nombre: "" } as Vet
-        case "pet": return { nombre: "" } as Owner
-        case "date": return { nombre: "" } as Owner
-        case "vaccine": return { nombre: "" } as Owner
+        case "pet": return { nombre: "" } as Pet
+        case "date": return { motivo: "" } as Cita
+        case "vaccine": return { nombre: "" } as Vaccine
         default: return null
     }
 }
@@ -17,7 +20,9 @@ function getType(name: string) {
 export async function GET(req: NextRequest, { params }: { params: { name: string } }) {
     try {
         const { name } = await params
-        const data = await get<unknown>(`/${name}`, req)
+        const typeObj = getType(name)
+        type reqT = typeof typeObj
+        const data = await get<reqT>(`/${name}`, req)
         return NextResponse.json(data)
     } catch (e) {
         return NextResponse.json({ message: e.message }, { status: 500 })
