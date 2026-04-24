6394cda15c6a9315c3a574774d2ade7a75c946a1
Yael <243678@ids.upchiapas.edu.mx>
2026-04-23 18:51:30 -0600

    desde hace rato funciona el endpoint pero el problema es que el front sí o sí quiere usar un header

--- src/app/api/vet/login/route.ts
+++ src/app/api/vet/login/route.ts
@@ -1,10 +1,14 @@
-import { Vet } from "@/interfaces/requests/vetRequest";
 import { get } from "@/lib/apiClient";
 import { NextRequest, NextResponse } from "next/server";
 
+export interface VetProfile {
+    id: number
+    nombre: string
+}
+
 export async function GET(req: NextRequest) {
     try {
-        const data = await get<Vet[]>('/veterinarios', req)
+        const data = await get<VetProfile[]>('/veterinarios', req)
     } catch (e) {
         return NextResponse.json({ message: e.message }, { status: 500 })
     }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,16 @@
+"use client"
+
 import SessionCard from "@/components/session_card";
+import { useEffect, useState } from "react";
+import { VetProfile } from "../api/vet/login/route";
 
 export default function Login() {
+    const [data, setData] = useState<VetProfile[]>([])
+    
+    useEffect(() => {
+        fetch('/api/vet/login').then(r => r.json()).then(setData).then(() => console.log(data))
+    })
+
     return (
         <div>
             <p>inicio d sesión</p>
