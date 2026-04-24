ca5f7ae7231c7924999ea9820d7721b5c07f6ea9
Yael <243678@ids.upchiapas.edu.mx>
2026-04-23 19:36:11 -0600

    ya obtiene los perfiles de veterinario, en la pagina de app solo estaba rara la indentacion

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,7 @@
 import SessionCard from "@/components/session_card";
 import { useEffect, useState } from "react";
 import { VetProfile } from "../api/vet/login/route";
+import Link from "next/link";
 
 export default function Login() {
     const [data, setData] = useState<VetProfile[]>([])
@@ -25,11 +26,20 @@ export default function Login() {
     }, [])
 
     return (
-        <div>
-            <p>inicio d sesión</p>
-            {data.map((profile) => (
-                <SessionCard key={profile.id} name={profile.nombre} route="" id={String(profile.id)}/>
-            ))}
-        </div>
+        <section className="flex justify-center items-center gap-25 my-auto">
+            <div>
+                <h1 className="text-2xl font-extrabold">¡Bienvenido a TuxMascotas!</h1>
+                <h2 className="text-xl font-semibold">Inicie sesión como:</h2>
+                <Link href={'/'} className="text-sm text-blue-600 hover:text-blue-500 hover:font-semibold">Regresar al menú principal</Link>
+            </div>
+                <section className="flex flex-col gap-5 w-100">
+                    {loading && (
+                        <p className="text-xl font-semibold">Cargando...</p>
+                    )}
+                    {data.map((profile) => (
+                    <SessionCard key={profile.id} name={profile.nombre} route="" id={String(profile.id)}/>
+                ))}
+                </section>
+            </section>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import SessionCard from "@/components/session_card";
 export default function Home() {
   return (
     <section className="flex justify-center items-center gap-25 my-auto">
-    <div>
-      <h1 className="text-2xl font-extrabold">¡Bienvenido a TuxMascotas!</h1>
-      <h2 className="text-xl font-semibold">Inicie sesión como:</h2>
-    </div>
-      <section className="flex flex-col gap-5 w-50">
-        <SessionCard name="Admin" route="/admin" id={null}/>
-        <SessionCard name="Recepción" route="/recep" id={null}/>
-        <SessionCard name="Veterinarios" route="/login" id={null}/>
-      </section>
+      <div>
+        <h1 className="text-2xl font-extrabold">¡Bienvenido a TuxMascotas!</h1>
+        <h2 className="text-xl font-semibold">Inicie sesión como:</h2>
+      </div>
+        <section className="flex flex-col gap-5 w-50">
+          <SessionCard name="Admin" route="/admin" id={null}/>
+          <SessionCard name="Recepción" route="/recep" id={null}/>
+          <SessionCard name="Veterinarios" route="/login" id={null}/>
+        </section>
     </section>
   );
 }
