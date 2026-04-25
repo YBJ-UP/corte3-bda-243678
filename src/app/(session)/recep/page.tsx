522828381f19209c27492325ad2c9aebae1421f8
Yael <243678@ids.upchiapas.edu.mx>
2026-04-24 19:39:30 -0600

    se añade el cuadro ese a las otras vistas, ya vi un error con el admin, no parece guardar el dia de descanso en el cache y truena

--- src/app/(session)/recep/page.tsx
+++ src/app/(session)/recep/page.tsx
@@ -1,14 +1,26 @@
+"use client"
+
+import ObjectEditor from "@/components/objectEditor";
 import ObjectViewer from "@/components/objectViewer";
 import { Cita } from "@/interfaces/requests/dateRequest";
 import { Owner } from "@/interfaces/requests/ownerRequest";
 import { Pet } from "@/interfaces/requests/petRequest";
-import { emptyCita, emptyOwner, emptyPet } from "@/lib/constants";
+import { emptyCita, emptyOwner, emptyPet, tablas } from "@/lib/constants";
+import { useState } from "react";
 
 export default function Reception() {
+    const [ editorOpen, setEditorOpen ] = useState<boolean>(false)
+    
     return (
         <section>
-            <section className="text-2xl">
-                <h2>OPCIONES:</h2>
+            <section >
+                <button type="button" onClick={() => setEditorOpen(true)} className="px-15 py-3 bg-green-600 rounded-2xl">Editar datos</button>
+                <ObjectEditor
+                    objects={[tablas.OWNER, tablas.PET, tablas.DATE]}
+                    permission={["add", "edit"]}
+                    isOpen={editorOpen}
+                    onClose={() => setEditorOpen(false)}
+                />
             </section>
             <section className="flex flex-col gap-10">
                 <h3 className="text-2xl">DATOS:</h3>
--- src/app/(session)/vet/page.tsx
+++ src/app/(session)/vet/page.tsx
@@ -1,20 +1,31 @@
+"use client"
+
+import ObjectEditor from "@/components/objectEditor";
 import ObjectViewer from "@/components/objectViewer";
 import { Cita } from "@/interfaces/requests/dateRequest";
-import { Owner } from "@/interfaces/requests/ownerRequest";
 import { Pet } from "@/interfaces/requests/petRequest";
-import { emptyCita, emptyOwner, emptyPet } from "@/lib/constants";
+import { Vaccine } from "@/interfaces/requests/vaccineRequest";
+import { emptyCita, emptyPet, emptyVaxx, tablas } from "@/lib/constants";
+import { useState } from "react";
 
 export default function Vet() {
+    const [ editorOpen, setEditorOpen ] = useState<boolean>(false)
     return (
         <section>
-            <section className="text-2xl">
-                <h2>OPCIONES:</h2>
+            <section>
+                    <button type="button" onClick={() => setEditorOpen(true)} className="px-15 py-3 bg-green-600 rounded-2xl">Editar datos</button>
+                    <ObjectEditor
+                        objects={[tablas.PET, tablas.DATE]}
+                        permission={["add", "edit"]}
+                        isOpen={editorOpen}
+                        onClose={() => setEditorOpen(false)}
+                    />
             </section>
             <section className="flex flex-col gap-10">
                 <h3 className="text-2xl">DATOS:</h3>
-                <ObjectViewer<Required<Owner>> object={emptyOwner} name="owner" alias="Dueños" />
                 <ObjectViewer<Required<Pet>> object={emptyPet} name="pet" alias="Mascotas registradas" />
                 <ObjectViewer<Required<Cita>> object={emptyCita} name="date" alias="Citas agendadas" />
+                <ObjectViewer<Required<Vaccine>> object={emptyVaxx} name="date" alias="Citas agendadas" />
             </section>
         </section>
             
